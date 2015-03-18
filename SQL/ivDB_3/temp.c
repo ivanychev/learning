d9aa957ea462dd70de085e041d6118f8b7ae6f7d
Sergey Ivanychev <sergeyivanychev@gmail.com>
2015-03-18 03:35:04 +0300

    Technical commit

---
+++ SQL/ivDB_3/temp.c
@@ -0,0 +1,17 @@
+#define values(a, b, c, date1, date2)\
+_values(a, b, c, 1, 2); $\
+insert into roles(person_id, team_id, role_id, season_id, league_id) _values(a, b, c, 2, 2)\
+
+insert into roles(person_id, team_id, role_id, season_id, league_id) values(51,17, 1,'08/02/2002','05/07/2008');
+insert into roles(person_id, team_id, role_id, season_id, league_id) values(52,17, 1,'09/05/2004','05/07/2009');
+insert into roles(person_id, team_id, role_id, season_id, league_id) values(53,17, 1,'05/04/2004',NULL);
+insert into roles(person_id, team_id, role_id, season_id, league_id) values(54,18, 1,'05/06/2001','05/08/2008');
+insert into roles(person_id, team_id, role_id, season_id, league_id) values(55,18, 1,'04/02/2002','05/08/2008');
+insert into roles(person_id, team_id, role_id, season_id, league_id) values(56,19, 1,'03/08/2002','05/07/2008');
+insert into roles(person_id, team_id, role_id, season_id, league_id) values(57,19, 1,'05/08/2003','05/08/2009');
+insert into roles(person_id, team_id, role_id, season_id, league_id) values(58,20, 1,'02/04/2004','05/08/2007');
+insert into roles(person_id, team_id, role_id, season_id, league_id) values(59,20, 1,'01/06/2003',NULL);
+insert into roles(person_id, team_id, role_id, season_id, league_id) values(60,20, 1,'05/03/2001','05/09/2009');
+insert into roles(person_id, team_id, role_id, season_id, league_id) values(61,21, 1,'07/06/2004','05/06/2009');
+insert into roles(person_id, team_id, role_id, season_id, league_id) values(62,21, 1,'05/02/2002','05/06/2008');
+insert into roles(person_id, team_id, role_id, season_id, league_id) values(63,21, 1,'09/04/2001',NULL);
