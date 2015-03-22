c346b7f6d4c6b7bc3d6a52bc0e4d5bd1ba4c2466
Sergey Ivanychev <sergeyivanychev@gmail.com>
2015-03-22 18:20:05 +0300

    Non-parallel version of determinant ready

--- Seminars/Determinant/ivmatrix.c
+++ Seminars/Determinant/ivmatrix.c
@@ -1,13 +1,31 @@
+#define DEBUG
+
 #include "ivmatrix.h"
 #include "iv_standard.h"
 
-int    get_matrix(int fd, matrix* this);
-double get_matrix_determinant(matrix* this);
-void   print_matrix(const matrix* this);
+int    	get_matrix(FILE* fd, matrix* this);
+double 	get_matrix_determinant(const matrix* this);
+void   	print_matrix(const matrix* this);
+void  	search_below_nzero_swap(matrix* this, uint32_t i);
+void  	annihilate_below 	    (matrix* this, uint32_t i);
+void  	swap_strings 	    (matrix* this, uint32_t first, uint32_t second);
+void  	add_string 	    (matrix* this, uint32_t added, uint32_t to, double factor);
+void  	gauss		    (matrix* this);
+void 	matrix_kill 	    (matrix* this);
 
-int main(int argc, char const *argv[]) {
+#undef  F_CHECK_EXIT_CODE
+#define F_CHECK_EXIT_CODE return -1;
+
+
+//======================================================================================
+
+int main(int argc, char const *argv[]) 
+{
+	printf("%d\n", argc);
+
 	CHECK(argc == 2, "Invalid number of arguments. Need only name of a file\n");
-	char* filename = argv[1];
+
+	const char* filename = argv[1];
 	FILE* fd = fopen(filename, "r");
 	CHECK(fd != NULL, "Failed to open matrix file");
 
@@ -17,14 +35,36 @@ int main(int argc, char const *argv[]) {
 		.data = NULL
 	};
 	int cond = get_matrix(fd, &current);
-	CHECK(cond == -1, "Failed to read matrix from file\n");
-	double determinant = get_matrix_determinant(&current);
+
+#undef  F_CHECK_EXIT_CODE
+#define F_CHECK_EXIT_CODE matrix_kill(&current); return -1;
+
+	CHECK(cond != -1, "Failed to read matrix from file\n");
+
 	print_matrix(&current);
-	printf("Determinant is %lg", determinant);
+	double determinant = get_matrix_determinant(&current);
+	printf("Determinant is %lg\n", determinant);
 	return 0;
 }
 
-int get_matrix(FILE* fd, matrix* this) {
+#undef  F_CHECK_EXIT_CODE
+#define F_CHECK_EXIT_CODE return -1;
+
+
+//======================================================================================
+
+void matrix_kill(matrix* this)
+{
+	if (this->data)
+	{
+		free(this->data);
+		this->data = NULL;
+	}
+	this->size = 0;
+}
+
+int get_matrix(FILE* fd, matrix* this) 
+{
 	CHECKN(fd   != NULL, IVPTRNULL);
 	CHECKN(this != NULL, IVPTRNULL);
 
@@ -43,10 +83,128 @@ int get_matrix(FILE* fd, matrix* this) {
 		CHECK(cond == 1, "Failed to read element");
 		((double*)(this->data))[i] = temp;
 	}
+	this->size = size;
 	return 0;
 }
 
 double get_matrix_determinant(const matrix* this)
 {
+	CHECKN(this != NULL, IVALLOCFAIL);
+	CHECK(this->data != NULL, "Invalid Matrix: no data");
+
+	matrix this_copy = *this;
+	gauss(&this_copy);
+	double result = 1.0;
+	for (uint32_t i = 0; i < this_copy.size; ++i)
+		result *= ELEM(&this_copy, i, i);
+	return result;	
+}
+
+
+void gauss(matrix* this)
+{
+	for (uint32_t i = 0; i < this->size; ++i)
+	{
+		if (IS_ZERO(ELEM(this, i, i)))
+			search_below_nzero_swap(this, i);
+		if (IS_ZERO(ELEM(this, i, i)))
+			continue;
+		annihilate_below(this, i);
+	}
+}
+
+void search_below_nzero_swap(matrix* this, uint32_t i)
+{
+	uint32_t j = i + 1;
+	for (; j < this->size; ++j)
+		if (!IS_ZERO(ELEM(this, j, i)))
+			break;
+	if (IS_ZERO(ELEM(this, j, i)))
+		return;
+	swap_strings(this, j, i);
+}
+
+//echo "Installing GetDeb" && sudo dpkg -i getdeb-repository_0.1-1~getdeb1_all.deb
+//echo "Installing PlayDeb" &&sudo dpkg -i playdeb_0.3-1~getdeb1_all.deb &&
+/*
+echo "Deleting Downloads" && rm -f getdeb-repository_0.1-1~getdeb1_all.deb && rm -f playdeb_0.3-1~getdeb1_all.deb
+*/
+
+inline void swap_strings(matrix* this, uint32_t first, uint32_t second)
+{
+	for (uint32_t i = 0; i < this->size; ++i)
+	{
+		double temp = ELEM(this, first, i);
+		ELEM(this, first, i) = ELEM(this, second, i);
+		ELEM(this, second, i) = temp;
+	}
+}
+
+/**
+ * @brief Annihilates numbers below [i, i] to zero (current is 1)
+ * @details 	1  2  3			1  2  3
+ * 		0 [3] 7 	-> 	0 [3] 7
+ * 		0  4  5 		0  0 (5 - 4*7/3)
+ * 
+ * @param this current matrix
+ * @param i position (1 in description)
+ */
+inline void annihilate_below(matrix* this, uint32_t i)
+{
+	for (uint32_t j = i + 1; j < this->size; ++j)
+		add_string(this, i, j, (-1.0)*ELEM(this, j, i)/ELEM(this, i, i));
+}
+
+inline void add_string(matrix* this, uint32_t added, uint32_t to, double factor)
+{
+	for (uint32_t i = 0; i < this->size; ++i)
+		ELEM(this, to, i) += ELEM(this, added, i) * factor;
+}
+
+#undef  F_CHECK_EXIT_CODE
+#define F_CHECK_EXIT_CODE return NULL;
+
+matrix* get_minor(const matrix* this, uint32_t string, uint32_t column)
+{
+	CHECKN(this != NULL, IVPTRNULL);
+	uint32_t old_amount = this->size * this->size;
+
+	void* new_data = calloc(old_amount, sizeof(double));
+	CHECKN(new_data != NULL, IVALLOCFAIL);
 	
+	void* old_data = this->data;
+
+#undef  F_CHECK_EXIT_CODE
+#define F_CHECK_EXIT_CODE free(new_data); return NULL;
+
+	uint32_t j = 0;
+	for (uint32_t i = 0; i < old_amount; ++i)
+	{
+		if (i % this->size == column || i / this->size == string)
+			continue;
+		((double*)new_data)[j] = ((double*)old_data)[i];
+		j++;
+	}
+
+	CHECK(j == (this->size - 1) * (this->size - 1), "Failed to copy numbers to minor");
+	matrix* new_matrix = (matrix*)calloc(1, sizeof(matrix));
+	CHECKN(new_matrix, IVALLOCFAIL);
+
+	new_matrix->size = this->size - 1;
+	new_matrix->data = new_data;
+	
+	return new_matrix;
+}
+
+#undef F_CHECK_EXIT_CODE
+
+void   print_matrix(const matrix* this)
+{
+	printf("Size = %"PRIu32"\n", this->size);
+	for (uint32_t i = 0; i < this->size; ++i) {
+		for (uint32_t j = 0; j < this->size; ++j)
+			printf("%4lg", ELEM(this, i, j));	// sorry for 4 tabs
+		putchar('\n');
+
+	}
 }
--- Seminars/Determinant/ivmatrix.h
+++ Seminars/Determinant/ivmatrix.h
@@ -4,16 +4,23 @@
 #include <fcntl.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include <math.h>
+#include <float.h>
 #include "stdio.h"
 #include "stdlib.h"
 
 struct matrix_s {
-	uint32_t size,
-	void* data
+	uint32_t size;
+	void* data;
 };
+
+#define ELEM_TYPE double
 
 //!	This macros allowes to get access to element like matrix[i][j] = 5;
 #define ELEM(matrix_ptr, string, column)	\
-	*(((double*)((matrix_ptr)->data)) + (string)*((matrix_ptr)->size) + (column))
+	*(((ELEM_TYPE*)((matrix_ptr)->data)) + (string)*((matrix_ptr)->size) + (column))
 
-typedef matrix_s matrix;
+#define IS_ZERO(num)				\
+	(fabs(num)<DBL_EPSILON)
+
+typedef struct matrix_s matrix;
