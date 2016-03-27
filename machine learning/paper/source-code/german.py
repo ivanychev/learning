Fixed data scaling. Added scoring for each dataset and each classifier

machine learning/paper/source-code/german.py
@@ -3,14 +3,14 @@ Module imports sample-feature matrix of German Credits data set from UCI
 
 Type:       classification
 Author:     Sergey Ivanychev
-Revision:   1
+Revision:   2
 """
 
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import normalize
+from sklearn.preprocessing import scale
 
-def load(path: str ="../data/statlog-german") \
+def load(path: str ="../data/statlog-german", raw=False) \
 -> (np.ndarray, np.ndarray):
     """
     :param path:    relative path to the folder that contains files such as
@@ -27,5 +27,6 @@ def load(path: str ="../data/statlog-german") \
 
     X = df[features_names].as_matrix()
     Y = df[target].as_matrix()
-
-    return normalize(X), Y, "german"
+    if raw==False:
+        X = scale(X)
+    return np.asfarray(X), Y, "german"

machine learning/paper/source-code/heart.py
@@ -5,15 +5,16 @@ This module uses only data collected in Cleveland, so the file
 
 Type:       classification, regression
 Author:     Sergey Ivanychev
-Revision:   1
+Revision:   2
 """
 
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import normalize
+from sklearn.preprocessing import scale
 
 
-def load(path: str ="../data/heart-disease", mode: str = "classification") \
+def load(path: str ="../data/heart-disease", mode: str = "classification", 
+    raw=False) \
 -> (np.ndarray, np.ndarray):
     """
     :param path:    string
@@ -40,4 +41,6 @@ def load(path: str ="../data/heart-disease", mode: str = "classification") \
     features.remove(target)
     X = df[features].as_matrix()
     Y = df[target].as_matrix()
-    return normalize(X), Y, "heart"
+    if raw==False:
+        X = scale(X)
+    return np.asfarray(X), Y, "heart"

machine learning/paper/source-code/housing.py
@@ -3,14 +3,14 @@ Module imports sample-feature matrix of housing, UCI
 
 Type:       classification
 Author:     Sergey Ivanychev
-Revision:   1
+Revision:   2
 """
 
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import normalize
+from sklearn.preprocessing import scale
 
-def load(path: str ="../data/housing") \
+def load(path: str ="../data/housing", raw=False) \
 -> (np.ndarray, np.ndarray):
     """
     :param path:    relative path to the folder that contains files such as
@@ -28,4 +28,6 @@ def load(path: str ="../data/housing") \
 
     X = df[features_names].as_matrix()
     Y = df[target].as_matrix()
-    return normalize(X), Y, "housing"
+    if raw==False:
+        X = scale(X)
+    return np.asfarray(X), Y, "housing"

machine learning/paper/source-code/ink.py
@@ -3,10 +3,10 @@ Implementing INK-spline kernel and related function-argument for SVC
 See DOCs for more info
 
 Author:     Sergey Ivanychev
-Revision:   2
+Revision:   3
 """
-from functools import partial
 
+from functools import partial
 import numpy as np
 from scipy.misc import comb
 
@@ -15,103 +15,46 @@ glob = 0
 def _is_integer(val):
     return isinstance(val, (int, np.int32, np.int64))
 
-def _sort_xy(x: np.ndarray, y: np.ndarray):
-    """
-    Gets (n, ) and (n, ) vectors X, Y and swaps i-th components if x[i] < y[i].
-    :param x:   (n, ) ndarray
-    :param y:   (n, ) ndarray
-    :return:    "swapped" X and Y
-    """
-    matrix = np.vstack((x, y))
-    matrix = np.sort(matrix, axis=1)
-    y = matrix[0].copy()
-    x = matrix[1].copy()
-    return x, y
+def K(X: np.ndarray, Y: np.ndarray, degree: int, a: int) -> np.ndarray:
+    gram = np.zeros((X.shape[0], Y.shape[0]))
+    verbose_len = X.shape[0]
+    for idx, x in enumerate(X):
+        S = np.zeros(Y.shape)
+        for r in range(degree + 1):
+            S += ((x - a)*(Y - a))**r +\
+            1.0/(2 * degree - r + 1) * comb(degree, r)*((np.abs(x - Y))**r)*(np.minimum(x, Y) - a)**(2*degree - r + 1)
+        gram_row = np.multiply.reduce(S, axis=1)
+        gram[idx] = gram_row
+        if idx % 500 == 0:
+            percentile = idx / verbose_len * 100
+            print("%f %% ready" % percentile)
+    return gram
 
-def _K(x: np.ndarray, y: np.ndarray, degree: int, a: int) -> float:
-    """
-    Implements not yet normalised multidimensional spline
-
-    :param x:          (n, ) ndarray
-    :param y:          (n, ) ndarray
-    :param degree:     int;  see ink()
-    :param a:          int;  see ink()
-    :return:           float; unnormalized kernel value
-    """
-    S = np.zeros(x.shape[0])
-    a = np.zeros(x.shape[0]) + a
-    x, y = _sort_xy(x, y)
-    for r in range(degree + 1):
-        S += np.power(x - a, r)*np.power(y - a, r) +\
-        1.0/(2 * degree - r + 1) * comb(degree, r)*np.power(np.abs(x - y), r)*np.power(y - a, 2*degree - r + 1)
-    return np.multiply.reduce(S)
-
-def _K_opt(x: np.ndarray, y: np.ndarray, degree: int, a: int) -> float:
-    """
-    Optimized _K()
-    :param x:
-    :param y:
-    :param degree:
-    :param a:
-    :return:
-    """
-    S = np.zeros(x.shape[0])
-#    a = np.zeros(x.shape[0]) + a
-    for i in np.where(x < y):
-        x[i], y[i] = y[i], x[i]
-    for r in range(degree + 1):
-        S += ((x - a)*(y - a))**r +\
-        1.0/(2 * degree - r + 1) * comb(degree, r)*((x - y)**r)*(y - a)**(2*degree - r + 1)
-    return np.multiply.reduce(S)
-
-def ink_opt(x: np.ndarray, y: np.ndarray, degree: int, a: int = -3) -> float:
-    """
-    Optimized ink(). Uses _K_opt()
-    :param x:
-    :param y:
-    :param degree:
-    :param a:
-    :return:
-    """
-    # assert _is_integer(degree) and degree > 0, "Degree must be positive integer"
-    # assert isinstance(x, np.ndarray) and isinstance(y, np.ndarray), "X and Y must be numpy arrays"
-    # assert len(x.shape) == len(y.shape) == 1, "X and Y must have only one dimension"
-
-    return _K_opt(x, y, degree, a) / np.sqrt(_K_opt(x, x, degree, a) * _K_opt(y, y, degree, a))
+def K_norm(X: np.ndarray, degree: int, a: int, axis: str) -> np.ndarray:
+    assert axis == "x" or axis =="y", "Invalid axis argument"
+    gram = np.zeros((X.shape[0], X.shape[0]))
+    for idx, x in enumerate(X):
+        S = np.zeros(x.shape)
+        for r in range(degree + 1):
+            S += ((x - a)*(x - a))**r +\
+            1.0/(2 * degree - r + 1) * comb(degree, r)*(0**r)*(x - a)**(2*degree - r + 1)
+        S = np.multiply.reduce(S)
+        gram[idx] = S
+    if axis == "x":
+        return gram
+    if axis == "y":
+        return np.transpose(gram)
 
 
-def ink(x: np.ndarray, y: np.ndarray, degree: int, a: int = -3) -> float:
-    """
-    Implements normalised multidimensional spline
-    :param x:       (n, ) ndarray
-    :param y:       (n, ) ndarray
-    :param degree:  int > 0         The degree of the spline being built
-    :param a:       the left side of the interval where the knots are located
-    :return:        computed kernel value
-    """
-    # assert _is_integer(degree) and degree > 0, "Degree must be positive integer"
-    # assert isinstance(x, np.ndarray) and isinstance(y, np.ndarray), "X and Y must be numpy arrays"
-    # assert len(x.shape) == len(y.shape) == 1, "X and Y must have only one dimension"
+def ink(x: np.ndarray, y: np.ndarray, degree: int, a: int = -3) -> np.ndarray:
+    assert _is_integer(degree) and degree > 0, "Degree must be positive integer"
+    assert isinstance(x, np.ndarray) and isinstance(y, np.ndarray), "X and Y must be numpy arrays"
 
-    return _K(x, y, degree, a) / np.power(_K(x, x, degree, a) * _K(y, y, degree, a), 1/2)
+    if len(x.shape) == 1:
+        x = x.reshape(1, x.shape[0])
+    if len(y.shape) == 1:
+        y = y.reshape(1, y.shape[0])
+    return K(x, y, degree, a) / np.sqrt(K_norm(x, degree, a, "x") * K_norm(y, degree, a, "y"))
 
-def ink_svc_get(degree: int, a: int = -3):
-    """
-    SVC-argument custom kernel function generator
-    :param degree: see ink()
-    :param a: see ink()
-    :return: ink_svc() function with custom degree and a parameters built in
-    """
-
-    def proxy_kernel(X, Y, K, **kwargs):
-        gram_matrix = np.zeros((X.shape[0], Y.shape[0]))
-        glob = 0
-        for i, x in enumerate(X):
-            for j, y in enumerate(Y):
-                gram_matrix[i, j] = K(x, y, **kwargs)
-                glob += 1
-                if glob % 100000 == 0:
-                    print("%s%% INK-spline calculated" % str(glob / gram_matrix.size * 100))
-        return gram_matrix
-
-    return partial(proxy_kernel, K=ink_opt, degree=degree, a=a)
+def get_sklearn_ink(degree: int, a: int):
+    return partial(K, degree=degree, a=a)

machine learning/paper/source-code/main.py
@@ -2,82 +2,15 @@
 The major routines implemented here
 
 Author:     Sergey Ivanychev
-Revision:   2
+Revision:   3
 """
 
+import utils
+from utils import test_classifier
+from utils import test_classifiers
 import sys
-import german
-import heart
-import wine
-from sklearn.cross_validation import KFold
-# import random
-import classifiers
-from itertools import combinations
-
-
-def get_data():
-    """
-    Loads data from processed sources
-    :return: list of (X, y) tuples
-    """
-    data = []
-    data.append(german.load())
-    data.append(heart.load())
-    data.append(wine.load())
-
-    data_dict = {}
-    for dataset in data:
-        data_dict[dataset[2]] = (dataset[0], dataset[1])
-    return data_dict
-
-# def subsets(n):
-#     nums = list(range(n))
-#     subsets = []
-#     for i in range(1, n):
-#         subsets += list(combinations(nums, i))
-#     return subsets
-
-def get_classifier_specs():
-    """
-    Procedure specifies interesting kernels that are observed in the project.
-    If you want to change the set of classifiers, you are welcome to edit this function.
-    Specs format available in docs and classifiers.py module
-    :return: list of kernel specs, that are processed in get_classifiers()
-    """
-    specs = []
-    specs.append(["linear"])
-    specs.append(["poly", 2])
-    specs.append(["poly", 3])
-    specs.append(["ink", 1, -3])
-    specs.append(["ink", 2, -3])
-    for i in [0.0001, 0.001, 0.01, 0.1, 1]:
-        specs.append((["rbf", i]))
-    return specs
-
-
-# def classifiers_subset_generator():
-#     specs = get_classifier_specs()
-#     sets = subsets(len(specs))
-#     random.shuffle(sets)
-#     for index_set in sets:
-#         current_specs = [specs[i] for i in index_set]
-#         clfs = classifiers.get(current_specs)
-#         yield clfs
-
-def get_margins(clfs, X_train):
-    ...
-
-def analyse(dataset, folds):
-    specs = get_classifier_specs()
-    clfs = classifiers.get(specs)
-    scores = [[] for i in range(len(clfs))]
-    for train, test in folds:
-        X_train, X_test = dataset[0][train], dataset[0][test]
-        Y_train, Y_test = dataset[1][train], dataset[1][test]
-        for idx, clf in enumerate(clfs):
-            clf.fit(X_train, Y_train)
-            scores[idx].append(clf.score(X_test, Y_test))
-        aggregator_data = get_margins(clfs, X_train)
+import ink
+import numpy as np
 
 
 
@@ -85,11 +18,22 @@ def dataset_size(dataset):
     return dataset[0].shape[0]
 
 def main(argv):
-    databank = get_data()
-    for dataset in databank:
-        folds = KFold(dataset_size(dataset), n_folds=5, shuffle=True, random_state=42)
-        analyse(dataset, folds)
+    clfs = utils.get_all_classifiers(spec_verbose=False)
+    clf = clfs[4]
 
+    X, y, _ = utils.get_dataset("housing")
+    train_idx = list(range(405))
+    test_idx = list(range(405, 506))
+    X_train, X_test = utils.scale(X, train_idx, test_idx)
+    print(np.min(X_train, axis=0))
+    clf.fit(X_train, y[train_idx])
+    clf.predict(X_test)
+
+    # X, y = test_classifier("housing", clf)
+    # print(np.minimum(X, axis=0))
+    # K = ink.get_sklearn_ink(degree=1, a=-3)
+    # print(K(X, X))
+    return 0
 
 
 

machine learning/paper/source-code/spam.py
@@ -8,9 +8,9 @@ Revision:   1
 
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import normalize
+from sklearn.preprocessing import scale
 
-def load(path: str ="../data/spambase") \
+def load(path: str ="../data/spambase", raw=False) \
 -> (np.ndarray, np.ndarray):
     """
     :param path:    relative path to the folder that contains files such as
@@ -27,4 +27,6 @@ def load(path: str ="../data/spambase") \
 
     X = df[features_names].as_matrix()
     Y = df[target].as_matrix()
-    return normalize(X), Y, "spambase"
+    if raw==False:
+        X = scale(X)
+    return np.asfarray(X), Y, "spambase"

machine learning/paper/source-code/utils.py
@@ -4,6 +4,19 @@ import german
 import spam
 import housing
 import numpy as np
+import classifiers
+from sklearn.cross_validation import KFold
+from sklearn.cross_validation import StratifiedKFold
+import sklearn
+from sklearn.preprocessing import StandardScaler
+
+DATA_MODULES = {
+    "heart" : heart,
+    "wine": wine,
+    "german": german,
+    "spambase": spam,
+    "housing": housing,
+}
 
 def get_classifier_specs():
     """
@@ -17,27 +30,38 @@ def get_classifier_specs():
     specs.append(["poly", 2])
     specs.append(["poly", 3])
     specs.append(["poly", 4])
-    # specs.append(["ink", 1, -3])
-    # specs.append(["ink", 2, -3])
+    specs.append(["ink", 1, -3])
+    specs.append(["ink", 2, -3])
     for i in [0.0001, 0.001, 0.01, 0.1, 1]:
         specs.append((["rbf", i]))
     return specs
 
-def get_data():
+def get_all_classifiers(C=0.1, spec_verbose=False):
+    specs = get_classifier_specs()
+    clfs = classifiers.get(specs, C=C)
+    if spec_verbose == True:
+        clfs = list(map(lambda x: (clfs[x], classifiers.spec_repr(specs[x])),
+                    range(len(specs))))
+    return clfs
+
+def get_data(raw=False):
     """
     Loads data from processed sources
     :return: list of (X, y) tuples
     """
     data = []
-    data.append(german.load())
-    data.append(heart.load())
-    data.append(wine.load())
-    data.append(spam.load())
-    data.append(housing.load())
+    data.append(german.load(raw=raw))
+    data.append(heart.load(raw=raw))
+    data.append(wine.load(raw=raw))
+    data.append(spam.load(raw=raw))
+    data.append(housing.load(raw=raw))
     data_dict = {}
     for dataset in data:
         data_dict[dataset[2]] = (dataset[0], dataset[1])
     return data_dict
+
+def get_dataset(name, raw=False):
+    return DATA_MODULES[name].load(raw=raw)
 
 def train_clfs(clfs, X_train, Y_train):
     for clf in clfs:
@@ -49,3 +73,64 @@ def get_margins(clfs, X_train):
     for idx, clf in enumerate(clfs):
         margins[:, idx] = clf.decision_function(X_train)
     return margins
+
+def scale(X, train_idx, test_idx):
+    scaler = StandardScaler()
+    scaler.fit(X[train_idx])
+    return scaler.transform(X[train_idx]), scaler.transform(X[test_idx])
+
+SCORERS = {
+    "accuracy": sklearn.metrics.accuracy_score
+}
+
+def _get_data(dataset):
+    if isinstance(dataset, str):
+        load_data = DATA_MODULES[dataset].load
+        X, y, _ = load_data(raw=True)
+    elif isinstance(dataset, tuple):
+        X, y = dataset[0], dataset[1]
+    else:
+        raise ValueError("Invalid dataset format")
+    return X, y
+
+def _get_cv(folds_type, folds, X, y, random_state=None):
+    if folds_type == "simple":
+        cv = KFold(X.shape[0], folds, random_state=random_state)
+    elif folds_type == "stratified":
+        cv = StratifiedKFold(y, folds, random_state=random_state)
+    else:
+        raise ValueError("Invalid folds type received")
+    return cv
+
+def test_classifier(dataset, clf, scoring_type="accuracy", folds=5, folds_type="simple"):
+    X, y = _get_data(dataset)
+    cv = _get_cv(folds_type, 5, X, y)
+    scores = []
+    scorer = SCORERS[scoring_type]
+    for train_idx, test_idx in cv:
+        X_train, X_test = scale(X, train_idx, test_idx)
+        clf.fit(X_train, y[train_idx])
+        scores.append(scorer(clf.predict(X_test), y[test_idx]))
+    scores = np.array(scores)
+    return np.mean(scores), np.std(scores), scores
+
+def _get_dataset_name(dataset):
+    if isinstance(dataset, str):
+        return dataset
+    dataset_name = None
+    try:
+        dataset_name = dataset[2]
+    except IndexError as a:
+        raise ValueError("Invalid dataset format")
+    return dataset_name
+
+
+def test_classifiers(dataset, scoring_type="accuracy", folds=5, folds_type="simple"):
+    dataset_name = _get_dataset_name(dataset)
+    print("Testing %s dataset...\nFolds = %d, Folds type = %s" % (dataset_name, folds, folds_type))
+    clfs = get_all_classifiers(spec_verbose=True)
+    for clf in clfs:
+        score, std_score, scores = test_classifier(dataset, clf[0])
+        print(clf[1])
+        print("Score = %f, std = %f" % (score, std_score))
+    print("Testing %s dataset finished\n" % dataset_name)

machine learning/paper/source-code/wine.py
@@ -9,9 +9,9 @@ Revision:   1
 
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import normalize
+from sklearn.preprocessing import scale
 
-def load(path: str ="../data/wine", mode: str = "classification") \
+def load(path: str ="../data/wine", mode: str = "classification", raw=False) \
 -> (np.ndarray, np.ndarray):
     df = pd.read_csv(path + "/winequality-white.csv", sep=";")
     cols = list(df.columns.values)
@@ -21,4 +21,6 @@ def load(path: str ="../data/wine", mode: str = "classification") \
     Y = df[target].as_matrix()
     if mode == "classification":
         Y = np.int32(Y >= 6)
-    return normalize(X), Y, "wine"
+    if raw==False:
+        X = scale(X)
+    return np.asfarray(X), Y, "wine"