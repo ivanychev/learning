0c54217b54b2a1ef7a026a66684313a839a2dca1
Sergey Ivanchev <sergeyivanychev@gmail.com>
2016-11-23 14:50:55 +0300

    Reorganised Python/ folder

--- Python/Yandex/first_attempt/001.py
+++
@@ -1,5 +0,0 @@
-with open("input.txt") as f:
-    string = f.readline()
-    nums = map(lambda x: int(x), string.split(" "))
-    with open("output.txt", "w") as out:
-        out.write(str(sum(nums)))
--- Python/Yandex/first_attempt/002.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-string = sys.stdin.readline()
-nums = map(lambda x: int(x), string.split())
-sys.stdout.write(str(sum(nums)))
